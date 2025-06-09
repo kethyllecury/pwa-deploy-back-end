2d50f35a312e29ca4bdb21b171da79b18cea59c1
kethyllecury <165853238+kethyllecury@users.noreply.github.com>
2025-06-08 23:38:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');  // Importa o middleware cors
+const cors = require('cors');
 const complaintsRoute = require('./routes/complaints');
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());  // Ativa o CORS para todas as rotas
-
-// Caso fosse limitar o acesso do CORS
-// const corsOptions = {
-//     origin: 'http://127.0.0.1:5500',
-//     optionsSuccessStatus: 200
-// };
-// app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI);
+const port = process.env.PORT || 10000;
 
-app.use('/api/complaints', complaintsRoute);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB conectado com sucesso!');
+    
+    app.use('/api/complaints', complaintsRoute);
+    
+    app.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao conectar no MongoDB:', err);
+  });
